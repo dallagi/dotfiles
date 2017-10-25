6b312b8d4b53ee7bd771a70610d33b908f413c32
Marco Dalla G <marco.dallagiacoma@gmail.com>
2017-10-25 10:29:34 +0200

    Quicklook plugins

--- scripts/macos_setup.sh
+++ scripts/macos_setup.sh
@@ -7,5 +7,8 @@ echo "+ Configuring Macos-specific stuff"
 
 brew install md5sha1sum
 
+echo "- Installing quick look plugins"
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+
 # todo: additional config
 
