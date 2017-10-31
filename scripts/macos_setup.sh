951cb86e317c8f2fce826a575d8b59f8a6e26b27
Marco Dalla G <marco.dallagiacoma@gmail.com>
2017-10-31 11:41:02 +0100

    Disable autocorrect in macos

--- scripts/macos_setup.sh
+++ scripts/macos_setup.sh
@@ -10,5 +10,8 @@ brew install md5sha1sum
 echo "- Installing quick look plugins"
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
+echo "- Setting sane defaults"
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
 # todo: additional config
 
