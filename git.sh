2ed558128be8d5060712e4b4e37e65e3f1c7cdda
Marco Dallagiacoma <marco.dallagiacoma@xpeppers.com>
2020-10-30 20:25:39 +0100

    Reduce verbosity of stow

--- git.sh
+++ git.sh
@@ -3,6 +3,6 @@ set -e
 read -r -p "+++ Linking .gitconfig and .gitignore +++"
 
 echo "+ Linking..."
-stow -v 5 --restow --dir files --target ~ git
+stow -v --restow --dir files --target ~ git
 
 echo "+ Done."
