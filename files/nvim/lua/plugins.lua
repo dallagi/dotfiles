8c9513ae46a3e31edfac87d74ea4015157df3578
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-07-03 17:38:21 +0200

    Move packer autoinstall to plugins.lua

--- files/nvim/lua/plugins.lua
+++ files/nvim/lua/plugins.lua
@@ -1,3 +1,12 @@
+-- Auto install packer.nvim if not exists
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+
+-- Configure plugins
 return require('packer').startup(function()
   -- Packer can manage itself as an optional plugin
   use {'wbthomason/packer.nvim', opt = true}
