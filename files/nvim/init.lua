6b8459d0d8f0b42dba9b1d3675edb43aa290b0e0
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-07-03 18:22:40 +0200

    Set leader to space

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -1,5 +1,11 @@
 local fn = vim.fn
 local execute = vim.api.nvim_command
 
+-- Set leader key to space
+vim.api.nvim_set_keymap('n', ' ', '<NOP>',
+		    {noremap = true, silent = true})
+vim.g.mapleader = ' '
+
+-- Import everything
 require('keymappings')
 require('plugins')
