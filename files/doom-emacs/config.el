25d1c269238ac7623bed7e3f14871cb33ccadbec
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2022-01-31 18:08:36 +0100

    Minor changes to doom emacs config

--- files/doom-emacs/config.el
+++ files/doom-emacs/config.el
@@ -27,7 +27,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-vibrant)
-(setq doom-theme 'doom-solarized-dark)
+(setq doom-theme 'doom-material)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
