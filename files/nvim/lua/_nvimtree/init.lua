837e032c647eb420a7e04211404d5d391c401eff
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2022-05-11 19:51:12 +0200

    Also remove nvim config

--- files/nvim/lua/_nvimtree/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.config = function()
-    vim.g.nvim_tree_side = "left"
-    vim.g.nvim_tree_width = "100%"
-    vim.g.nvim_tree_quit_on_open = 1
-end
-
-return M
--- files/nvim/lua/_onedark/init.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.config = function()
-  require("onedark").setup()
-end
-
-return M
