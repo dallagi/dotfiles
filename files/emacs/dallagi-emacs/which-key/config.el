b06f588119c00c318f2be95d72fb2497e5b60d24
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2022-05-11 19:47:27 +0200

    Cleanup dotfiles

--- files/emacs/dallagi-emacs/which-key/config.el
+++
@@ -1,6 +0,0 @@
-(defun which-key-configure-groups-description ()
-  ;; describe which-key groups
-  (which-key-add-key-based-replacements
-    "SPC c" "code"
-    "SPC w" "window"
-    ))
--- fish/aliases.fish
+++
@@ -1,6 +0,0 @@
-alias updatedb="sudo /usr/libexec/locate.updatedb" 
-alias v=vagrant
-alias vim=nvim
-
-# Work stuff
-alias arr="docker-compose run --rm admin"
