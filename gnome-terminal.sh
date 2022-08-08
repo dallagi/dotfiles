210fbdda6841d92578db3b1e7d313c27cb7d00cf
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2022-08-08 08:38:04 +0200

    Update gnome-terminal

--- gnome-terminal.sh
+++ gnome-terminal.sh
@@ -2,10 +2,13 @@ set -e
 
 read -r -p "+++ Installing one-dark theme for gnome-terminal +++"
 
-echo "+ Cloning repo..."
-git clone https://github.com/aaron-williamson/base16-gnome-terminal.git ~/.config/base16-gnome-terminal
+echo "+ Downloading script..."
+curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh > /tmp/gnome-terminal-one-dark-theme.sh
 
-echo "+ Installing Onedark theme..."
-~/.config/base16-gnome-terminal/color-scripts/base16-onedark.sh
+read -r -p "+ Time to check the script, press enter when ready"
+vi /tmp/gnome-terminal-one-dark-theme.sh
+
+read -r -p "+ Press enter to install. Ctrl-C to abort"
+bash /tmp/gnome-terminal-one-dark-theme.sh
 
 echo "+ Theme installed. Activate it from the Gnome-Terminal settings."
