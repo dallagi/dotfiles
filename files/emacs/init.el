3d47f291e2c5f473882a121c15b509d66fcf3954
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-02-04 20:19:24 +0100

    Cleanup init.el

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,17 +1,2 @@
-(org-babel-load-file "configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-tomorrow-night))
- '(custom-safe-themes
-   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
- '(package-selected-packages
-   '(no-littering general magit evil-collection org-modern evil-org color-theme-sanityinc-tomorrow)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq vc-follow-symlinks t) ;; necessary as emacs config is symlinked
+(org-babel-load-file (locate-user-emacs-file "configuration.org"))
