a4751a73d883dcb7aa8b4cb2eab67d9bae9f19a8
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-02-20 10:03:44 +0100

    Emacs config script

--- emacs.sh
+++ emacs.sh
@@ -1,1 +1,8 @@
-echo "Not yet implemented"
+set -e
+
+read -r -p "+++ Linking emacs config +++"
+mkdir -p ~/.config/emacs
+stow --restow --dir files --target ~/.config/emacs emacs
+
+echo "+ Done."
+
